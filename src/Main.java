d4af7a37530d5cfe20ca05faa94832a93b73a4d1
Maksim T <verdo.ya.ry19@gmail.com>
2023-05-16 19:25:35 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 15737;
+        int y = 20;
+
+        System.out.println("Клиент получит "+ x/ y + " бонусов");
+        }
+    }
